91fc8acd23bdf9a73f4ea644b474f9a1bbf2a00d
Lopes <gus.lcampos38@gmail.com>
2021-12-14 23:12:10 -0300

    implement arithmatics engine

--- back-end/controller/walls/post.js
+++ back-end/controller/walls/post.js
@@ -5,19 +5,31 @@ module.exports = async (req, res, next) => {
   try {
     const {
       wallOneHeight, wallOneLength,
+      doorsOne, windowsOne,
       wallTwoHeight, wallTwoLength,
+      doorsTwo, windowsTwo,
       wallThreeHeight, wallThreeLength,
+      doorsThree, windowsThree,
       wallFourHeight, wallFourLength,
+      doorsFour, windowsFour,
     } = req.body;
     const dimensions = {
       wallOneHeight,
       wallOneLength,
+      doorsOne,
+      windowsOne,
       wallTwoHeight,
       wallTwoLength,
+      doorsTwo,
+      windowsTwo,
       wallThreeHeight,
       wallThreeLength,
+      doorsThree,
+      windowsThree,
       wallFourHeight,
       wallFourLength,
+      doorsFour,
+      windowsFour,
     };
 
     return res.status(200).json({ total: calcArea(dimensions) });
--- back-end/service/calcArea.js
+++ back-end/service/calcArea.js
@@ -1,15 +1,26 @@
 const calcArea = (info) => {
+  const doorArea = 1.52;
+  const windowArea = 2.4;
+
   const {
     wallOneHeight, wallOneLength,
+    doorsOne, windowsOne,
     wallTwoHeight, wallTwoLength,
+    doorsTwo, windowsTwo,
     wallThreeHeight, wallThreeLength,
+    doorsThree, windowsThree,
     wallFourHeight, wallFourLength,
+    doorsFour, windowsFour,
   } = info;
 
-  const area = Number(wallOneHeight) * Number(wallOneLength)
+  const area = (Number(wallOneHeight) * Number(wallOneLength)
   + Number(wallTwoHeight) * Number(wallTwoLength)
   + Number(wallThreeHeight) * Number(wallThreeLength)
-  + Number(wallFourHeight) * Number(wallFourLength);
+  + Number(wallFourHeight) * Number(wallFourLength))
+  - (Number(doorsOne) * doorArea + Number(windowsOne) * windowArea
+    + Number(doorsTwo) * doorArea + Number(windowsTwo) * windowArea
+    + Number(doorsThree) * doorArea + Number(windowsThree) * windowArea
+    + Number(doorsFour) * doorArea + Number(windowsFour) * windowArea);
 
   return area;
 };

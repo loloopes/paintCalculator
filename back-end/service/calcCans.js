8b1cd00f791914caf0bb903354a826c3bc9ac853
Lopes <gus.lcampos38@gmail.com>
2021-12-15 22:15:24 -0300

    adjust can calculator

--- back-end/service/calcCans.js
+++ back-end/service/calcCans.js
@@ -27,32 +27,32 @@ const calcCans = (info) => {
   let medium = null;
   let small = null;
 
-  xLarge = parseInt(area / 18, 10);
+  xLarge = parseInt(area / (18 * 5), 10);
 
-  if (xLarge) area -= xLarge * 18;
+  if (xLarge) area -= xLarge * (18 * 5);
 
-  large = parseInt(area / 3.6, 10);
+  large = parseInt(area / (3.6 * 5), 10);
 
-  if (large) area -= large * 3.6;
+  if (large) area -= large * (3.6 * 5);
 
-  medium = parseInt(area / 2.5, 10);
+  medium = parseInt(area / (2.5 * 5), 10);
 
-  if (medium) area -= medium * 2.5;
+  if (medium) area -= medium * (2.5 * 5);
 
-  small = area % 0.5 > 0 ? parseInt(area / 0.5, 10) + 1 : area / 0.5;
+  small = area % 0.5 > 0 ? parseInt(area / (0.5 * 5), 10) + 1 : area / (0.5 * 5);
 
-  const cans = small === 5 ? {
-    xLarge, large, medium: 1, small: 0,
-  } : {
-    xLarge, large, medium, small,
-  };
-
-  // const cans = {
-  //   xLarge,
-  //   large,
-  //   medium,
-  //   small,
+  // const cans = small === 5 ? {
+  //   xLarge, large, medium: 1, small: 0,
+  // } : {
+  //   xLarge, large, medium, small,
   // };
+
+  const cans = {
+    xLarge,
+    large,
+    medium,
+    small,
+  };
 
   return cans;
 };

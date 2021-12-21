3368e03cc6ff577d94e35fb3ccd3a5293766f74f
Lopes <gus.lcampos38@gmail.com>
2021-12-21 00:46:00 -0300

    create walls table

--- front-end/src/pages/MainPage.jsx
+++ front-end/src/pages/MainPage.jsx
@@ -1,33 +1,18 @@
-/* eslint-disable */
-import React, {useState} from 'react';
+/* eslint-disable react/no-array-index-key */
+/* eslint-disable react/function-component-definition */
+import React, { useState } from 'react';
 import './MainPage.css';
 import postData from '../services/postData';
 import LabelInput from '../components/LabelInput';
 import Modal from '../components/Modal';
 
-
 export default function MainPage() {
-  const [wallOneHeight, setWallOneHeight] = useState();
-  const [wallOneLength, setWallOneLength] = useState();
-  const [doorsOne, setDoorsOne] = useState(0);
-  const [windowsOne, setWindowsOne] = useState(0);
+  const [wallHeight, setWallHeight] = useState();
+  const [wallLength, setWallLength] = useState();
+  const [doors, setDoors] = useState(0);
+  const [windows, setWindows] = useState(0);
 
-  const [wallTwoHeight, setWallTwoHeight] = useState();
-  const [wallTwoLength, setWallTwoLength] = useState();
-  const [doorsTwo, setDoorsTwo] = useState(0);
-  const [windowsTwo, setWindowsTwo] = useState(0);
-  
-  const [wallThreeHeight, setWallThreeHeight] = useState();
-  const [wallThreeLength, setWallThreeLength] = useState();
-  const [doorsThree, setDoorsThree] = useState(0);
-  const [windowsThree, setWindowsThree] = useState(0);
-
-  const [wallFourHeight, setWallFourHeight] = useState();
-  const [wallFourLength, setWallFourLength] = useState();
-  const [doorsFour, setDoorsFour] = useState(0);
-  const [windowsFour, setWindowsFour] = useState(0);
-
-  const [walls, setWalls] = useState([])
+  const [walls, setWalls] = useState([]);
   const [cans, setCans] = useState({
     xLarge: 0,
     large: 0,
@@ -36,67 +21,75 @@ export default function MainPage() {
   });
   const [error, setError] = useState('');
 
-  let info = {
-    wallOneHeight, wallOneLength,
-    doorsOne, windowsOne,
-    wallTwoHeight, wallTwoLength,
-    doorsTwo, windowsTwo,
-    wallThreeHeight, wallThreeLength,
-    doorsThree, windowsThree,
-    wallFourHeight, wallFourLength,
-    doorsFour, windowsFour,
+  const info = {
+    wallHeight,
+    wallLength,
+    doors,
+    windows,
   };
 
   return (
     <div>
-      <form className='form'>
-        <div className='card'>
-          <Modal/>
-          <div className='card-body'>
-            <h5 className='card-title'>Info parede 1</h5>
-            <LabelInput entity='paredeA1' text='Altura Parede 1' handleChange={setWallOneHeight}/>
-            <LabelInput entity='paredeL1' text='Largura parede 1' handleChange={setWallOneLength}/>
-            <LabelInput entity='portasP1' text='Portas parede 1' handleChange={setDoorsOne}/>
-            <LabelInput entity='janelasP1' text='Janelas parede 1' handleChange={setWindowsOne}/>
+      {/* Table walls starts */}
+      <section>
+        <table className="table">
+          <thead>
+            <tr>
+              <th scope="col">#</th>
+              <th scope="col">wallHeight</th>
+              <th scope="col">wallLength</th>
+              <th scope="col">doors</th>
+              <th scope="col">windows</th>
+            </tr>
+          </thead>
+          <tbody>
+            {
+              walls.map((wall, index) => (
+                <tr>
+                  <th scope="row">{index + 1}</th>
+                  <td>{wall.wallHeight}</td>
+                  <td>{wall.wallLength}</td>
+                  <td>{wall.doors}</td>
+                  <td>{wall.windows}</td>
+                </tr>
+              ))
+            }
+          </tbody>
+        </table>
+
+      </section>
+
+      {/* Table ends */}
+
+      <form className="form">
+        <div className="card">
+          <Modal />
+          <div className="card-body">
+            <h5 className="card-title">Info parede</h5>
+            <LabelInput entity="paredeA1" text="Altura Parede 1" handleChange={setWallHeight} />
+            <LabelInput entity="paredeL1" text="Largura parede 1" handleChange={setWallLength} />
+            <LabelInput entity="portasP1" text="Portas parede 1" handleChange={setDoors} />
+            <LabelInput entity="janelasP1" text="Janelas parede 1" handleChange={setWindows} />
           </div>
-          <button type="button" className='btn btn-primary calculate' onClick={() => console.log('click')}>Adicionar</button>
+          <button
+            type="button"
+            className="btn btn-primary calculate"
+            onClick={() => setWalls([...walls, {
+              wallHeight, wallLength, doors, windows,
+            }])}
+          >
+            Adicionar
+          </button>
         </div>
-        {/* <div className='card'>
-          <div className='card-body'>
-            <h5 className='card-title'>Info parede 2</h5>
-            <LabelInput entity='paredeA2' text='Altura Parede 2' handleChange={setWallTwoHeight}/>
-            <LabelInput entity='paredeL2' text='Largura parede 2' handleChange={setWallTwoLength}/>
-            <LabelInput entity='portasP2' text='Portas parede 2' handleChange={setDoorsTwo}/>
-            <LabelInput entity='janelasP2' text='Janelas parede 2' handleChange={setWindowsTwo}/>
-          </div>
-        </div>
-        <div className='card'>
-          <div className='card-body'>
-            <h5 className='card-title'>Info parede 3</h5>
-            <LabelInput entity='paredeA3' text='Altura Parede3' handleChange={setWallThreeHeight}/>
-            <LabelInput entity='paredeL3' text='Largura parede3' handleChange={setWallThreeLength}/>
-            <LabelInput entity='portasP3' text='Portas parede 3' handleChange={setDoorsThree}/>
-            <LabelInput entity='janelasP3' text='Janelas parede 3' handleChange={setWindowsThree}/>
-          </div>
-        </div>
-        <div className='card'>
-          <div className='card-body'>
-            <h5 className='card-title'>Info parede 4</h5>
-            <LabelInput entity='paredeA4' text='Altura Parede: 4' handleChange={setWallFourHeight}/>
-            <LabelInput entity='paredeL4' text='Largura parede: 4' handleChange={setWallFourLength}/>
-            <LabelInput entity='portasP4' text='Portas parede 4: ' handleChange={setDoorsFour}/>
-            <LabelInput entity='janelasP4' text='Janelas parede 4: ' handleChange={setWindowsFour}/>
-          </div>
-        </div>    */}
       </form>
       <section>
-        <div className='card result'>
-          <h5 className='card-title'>Total latas</h5>
+        <div className="card result">
+          <h5 className="card-title">Total latas</h5>
           {Object.entries(cans).map(([size, quantity], index) => <p key={index}>{`${size}: ${quantity}`}</p>)}
           <p>{error}</p>
-          <button type="button" className='btn btn-primary calculate' onClick={() => postData(info, setCans, setError)}>Calcular</button>
+          <button type="button" className="btn btn-primary calculate" onClick={() => postData(info, setCans, setError)}>Calcular</button>
         </div>
       </section>
     </div>
-  )
-};
+  );
+}

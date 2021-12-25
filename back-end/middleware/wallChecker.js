97bf6cd4c06a7d37f6e5a48ccd12ad202a972e2e
Lopes <gus.lcampos38@gmail.com>
2021-12-25 19:51:10 -0300

    fix error bug

--- back-end/middleware/wallChecker.js
+++ back-end/middleware/wallChecker.js
@@ -7,28 +7,36 @@ module.exports = async (req, _res, next) => {
 
   const data = req.body;
 
-  const walls = data.map((wall) => ({
-    wallArea: Number(wall.wallHeight) * Number(wall.wallLength),
-    doorsWindowsArea: Number(wall.doors) * doorArea + Number(wall.windows) * windowArea,
+  const numberfiedData = data.map(({
+    wallHeight, wallLength, doors, windows,
+  }) => ({
+    numberWallHeight: Number(wallHeight),
+    numberWallLength: Number(wallLength),
+    numberDoors: Number(doors),
+    numberWindows: Number(windows),
+  }));
+
+  const walls = numberfiedData.map((wall) => ({
+    wallArea: wall.numberWallHeight * wall.numberWallLength,
+    doorsWindowsArea: wall.numberDoors * doorArea + wall.numberWindows * windowArea,
   }));
 
   const dataSchema = Joi.array().items(Joi.object({
-    wallHeight: Joi.alternatives().conditional('doors', { not: 0, then: Joi.number().min(2.2).required(), otherwise: Joi.number().min(0.00000000000001).required().messages({ 'number.base': 'Altura da parede deve ser um numero', 'number.min': 'Altura da parede deve ser maior que 0' }) }),
-    wallLength: Joi.number().min(0).required().messages({ 'number.min': 'largura deve ser maior que 0', 'number.base': 'largura deve ser um numero' }),
-    doors: Joi.number().min(0).required().messages({ 'number.base': 'portas deve ser um numero, mesmo que 0' }),
-    windows: Joi.number().min(0).required().messages({ 'number.min': 'area da parede deve ser maior que 0', 'number.base': 'Janelas deve ser um numero, mesmo que 0' }),
-
+    numberWallHeight: Joi.alternatives().conditional('numberDoors', { is: 0, then: Joi.number().min(0.0000001).required().messages({ 'number.empty': 'o campo altura da parede nao pode ser vazio', 'number.min': 'o campo altura da parede deve ser maior que 0' }), otherwise: Joi.number().min(2.2).required().messages({ 'number.empty': 'Altura da parede nao pode ser um campo vazio', 'number.min': 'Quando ha portas, paredes devem ser no minimo 2.2m' }) }),
+    numberWallLength: Joi.number().min(0.0000001).required().messages({ 'number.min': 'largura deve ser maior que 0', 'number.base': 'largura deve ser um numero' }),
+    numberDoors: Joi.number().min(0).required().messages({ 'number.min': 'portas deve ser um numero, mesmo que 0', 'number.empty': 'Campo portas nao pode ser vazio' }),
+    numberWindows: Joi.number().min(0).required().messages({ 'number.min': 'janelas deve ser um numero, mesmo que 0', 'number.base': 'Janelas deve ser um numero, mesmo que 0' }),
   }));
 
   const wallsSchema = Joi.array().items(Joi.object({
     wallArea: Joi.number().min(1).max(15).required()
-      .messages({ 'number.min': 'area da parede deve ser maior que 0', 'number.max': 'Area da parede nao pode exceder 15m²' }),
+      .messages({ 'number.min': 'area da parede deve no minimo 1m²', 'number.max': 'Area da parede nao pode exceder 15m' }),
     doorsWindowsArea: Joi.number().max(Joi.ref('wallArea', {
       adjust: (value) => value * 0.5,
     })).required().messages({ 'number.max': 'area de portas e janelas somadas devem corresponder a no maximo 50% da area de parede' }),
   }));
 
-  const dataCheck = dataSchema.validate(data);
+  const dataCheck = dataSchema.validate(numberfiedData);
 
   if (dataCheck.error) return next(dataCheck.error);
 
